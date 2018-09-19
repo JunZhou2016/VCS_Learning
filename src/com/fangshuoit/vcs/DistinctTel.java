50e86451b13cf552a8051451d2a49aa78f17e8ca
zhoujun <zhoujun1@icerno.com>
2018-09-19 16:35:37 +0800

    分支2文件提交。

--- src/com/fangshuoit/vcs/DistinctTel.java
+++ src/com/fangshuoit/vcs/DistinctTel.java
@@ -40,6 +40,7 @@ public class DistinctTel {
 		//红先
 		// 排序;
 		
+		//commit to branch2.0
 		//测试冲突解决方案;
 		// 注意基本数据类型不能使用Arrays.asList()方法将数组转化为list;
 		List<Character> numList = new ArrayList<Character>(numSet);
