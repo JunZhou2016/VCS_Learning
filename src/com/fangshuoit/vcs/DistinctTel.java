1a7ea6469aad4265d30eb736e6e12baa8be95420
zhoujun <zhoujun1@icerno.com>
2018-09-19 16:47:50 +0800

    1.000

--- src/com/fangshuoit/vcs/DistinctTel.java
+++ src/com/fangshuoit/vcs/DistinctTel.java
@@ -42,6 +42,8 @@ public class DistinctTel {
 		
 		//commit red to branch1
 		
+		//commit msg to branch1.000
+		
 		//测试冲突解决方案;
 		// 注意基本数据类型不能使用Arrays.asList()方法将数组转化为list;
 		List<Character> numList = new ArrayList<Character>(numSet);
