370efcf2ad297ef3622fc787dfc1cd91ab6a7be5
zhoujun <zhoujun1@icerno.com>
2018-09-19 16:08:45 +0800

    红先

--- src/com/fangshuoit/vcs/DistinctTel.java
+++ src/com/fangshuoit/vcs/DistinctTel.java
@@ -36,6 +36,8 @@ public class DistinctTel {
 
 		//201809191556
 		//201809191555
+		
+		//红先
 		// 排序;
 		// 注意基本数据类型不能使用Arrays.asList()方法将数组转化为list;
 		List<Character> numList = new ArrayList<Character>(numSet);
